655ba9fde7835f0fc64704779231644687dae81a
Yuvrajsinghspd09 <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-05-18 14:48:18 +0530

    Add files via upload

---
+++ BestTimetoBuyandSellStock.py
@@ -0,0 +1,27 @@
+#Optimized one with O(n)
+def maxProfit(prices):
+    if not prices or len(prices) == 1:
+        return 0
+
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices[1:]:
+        if price < min_price:
+            min_price = price
+        else:
+            max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+
+#Brute force for buying and selling stock with O(n^2)
+
+def maxProfit(prices):
+    max_profit = 0
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > max_profit:
+                max_profit = profit
+    return max_profit
