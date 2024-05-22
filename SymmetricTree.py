c73139b95afbf43a305d102dd6e004530cb2446b
Yuvrajsinghspd09 <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-05-22 21:22:09 +0530

    Update SymmetricTree.py

--- SymmetricTree.py
+++ SymmetricTree.py
@@ -1,3 +1,8 @@
+'''
+The time complexity of this solution is O(n), where n is the number of nodes in the binary tree, as each node is visited once in the worst case. The space complexity is O(h), where h is the height of the binary tree, due to the recursive calls on the function call stack.
+
+'''
+
 class Treenode:
     def __init__(self,val=0,right=None,left=None):
         self.val=val
