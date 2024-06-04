ab9adcbee894c22163a73a1db71fb14dc634ee0e
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-04 23:45:15 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -2,6 +2,32 @@
 
 '''
 
+When to use DFS and BFS:
+
+DFS:
+
+When you want to explore all possible paths in a graph or tree.
+When you want to traverse a graph or tree in a systematic way.
+When the graph or tree is relatively deep and you want to explore deep paths first.
+When you need to find connected components or cycles in an undirected graph.
+
+
+BFS:
+
+When you want to find the shortest path between two nodes in an unweighted graph.
+When you want to explore the neighbors of a node before moving to the next level.
+When the graph is relatively wide and you want to explore all neighbors first.
+When you need to find the minimum number of steps or distance between two nodes.
+
+
+
+Both DFS and BFS have a time complexity of O(V + E), where V is the number of vertices and E is the number of edges in the graph
+
+'''
+
+
+'''
+
 graph = {
     'A': ['B', 'C'],
     'B': ['A', 'D', 'E'],
