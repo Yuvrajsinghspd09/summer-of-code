40786fc5b10b444e3c956d167da0b8a3e8df7aa6
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-14 08:53:33 +0530

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -3,4 +3,14 @@ def rev_string(s):
     while left<right:
         s[left],s[right]=s[right],s[left]
         left+=1
-        right+=1
+        right-=1
+    return s
+#practice attempt 1
+def rev_string(s):
+    left=0
+    right=len(s)-1
+    while left<right:
+        s[left],s[right] =s[right],s[left]
+        left+=1
+        right-=1
+    return s
