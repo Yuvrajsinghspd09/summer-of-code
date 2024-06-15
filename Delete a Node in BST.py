adb6ca7c33d4a4dfe954766116cdfac4e6df415a
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-15 21:17:44 +0530

    Update Delete a Node in BST.py

--- Delete a Node in BST.py
+++ Delete a Node in BST.py
@@ -70,3 +70,40 @@ O(n)
 
 
 '''
+
+
+#practice attempt 1
+class treenode:
+    def bst(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+def deleteNode(root,key):
+    if root is None:
+        return root
+
+    if key<root.val:
+        root.left = deleteNode(root.left,key)
+    elif key>root.val:
+        root.right = deleteNode(root.right,key)
+    else: 
+        if root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+
+     # Node with two children: Get the inorder successor (smallest in the right subtree)
+        root.val = minValue(root.right)
+
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, root.val)
+
+    return root
+
+def minValue(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current.val
+        
