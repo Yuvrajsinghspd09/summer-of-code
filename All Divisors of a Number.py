53943e70fad5d256e3dbaaf3ad9ce5f4d3039370
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-16 14:02:05 +0530

    Update All Divisors of a Number.py

--- All Divisors of a Number.py
+++ All Divisors of a Number.py
@@ -17,3 +17,15 @@ def find_divisors(N):
         
     #The line print(" ".join(map(str, divisors))) is a compact and efficient way to convert a list of integers into a single string 
     # where each integer is separated by a space, and then print that string. 
+ # practice attempt 1
+ def find_all_div(num):
+     divisors=[]
+     for i in range (1,int(num**0.5)+1):
+          if num%i ==0:
+           divisors.append(i)
+
+           if i!=num//i:
+            divisors.append(num//i)
+          divisors.sort()
+          print(" ".join(map(str, divisors)))
+      
