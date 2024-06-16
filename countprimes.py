d2afb59521457e3b36e771f600dfce6bba752be1
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-16 19:42:57 +0530

    Update countprimes.py

--- countprimes.py
+++ countprimes.py
@@ -15,12 +15,14 @@ def countPrimes(n):
 # time complexity :O(N∗Log(Log(N)))
 #Space Complexity O(N)
 
-def count_prime_num(N):
-    if N<3:
+def count_prime(n):
+    if n<3:
         return 0
-    primes = [True]*N
-    for i in range(2,int(N**0.5)+1):
-        if primes[i] is True:
-            for j in range(i,int(N//2),i):
+    primes = [True]*n
+    primes[0]=primes[1]=False
+
+    for i in range(2,int(n**2)+1):
+        if primes[i]:
+            for j in range(i*i,n,i):
                 primes[j]=False
     return sum(primes)
