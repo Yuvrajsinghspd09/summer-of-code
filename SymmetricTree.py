87386591891a476733043952cc4f0a41ecfe34a2
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-18 18:42:03 +0530

    Update SymmetricTree.py

--- SymmetricTree.py
+++ SymmetricTree.py
@@ -18,3 +18,36 @@ def Is_symmetric(root:Treenode)->bool:
         return (t1.val==t2.val) and check_mirror(t1.right,t2.left) and check_mirror(t1.left,t2.right)
     
     return check_mirror(root,root)
+
+
+'''
+Here's a visual representation of how check_mirror(root, root) works:
+
+root
+             /     \
+           left    right
+          subtree  subtree
+
+check_mirror(root, root)
+  |
+  |-- check_mirror(left, right)
+        |
+        |-- check_mirror(left.left, right.right)
+        |-- check_mirror(left.right, right.left)
+'''
+
+#practice attempt 1
+class treenode(self,val=0,left=Nonw,right=None):
+    self.val=val
+    self.left=left
+    self.right=right
+
+def is_symm(root:treenode)->bool:
+    def check_mirror(t1:treenode,t2:treenode)->bool:
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+
+        return (t1.val==t2.val) and check_mirror(t1.right,t2.left) and check_mirror(t1.left,t2.right)
+    return check_mirror(root,root)
