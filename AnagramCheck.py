e3cb9fe5a86665a503d60521030f4f7dc7e4e68b
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-06-19 11:25:55 +0530

    Update AnagramCheck.py

--- AnagramCheck.py
+++ AnagramCheck.py
@@ -5,3 +5,6 @@ def isAnagram(s, t):
 An anagram is formed by rearranging the letters of a word or phrase.
  To check if two strings are anagrams, we can compare their sorted versions
 '''
+# practice attempt 1
+def anagram_check(word1,word2):
+    return sorted(word1)==sorted(word2)
