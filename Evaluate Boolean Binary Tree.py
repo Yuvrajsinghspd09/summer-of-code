e37ad13616aa82ad0ec0e470160c31e3881c0c2f
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-02 19:49:42 +0530

    Update Evaluate Boolean Binary Tree.py

--- Evaluate Boolean Binary Tree.py
+++ Evaluate Boolean Binary Tree.py
@@ -1,3 +1,12 @@
+'''
+not test case
+return root.val == 1 effectively does the following:
+
+For a leaf node with value 1, it returns True
+For a leaf node with value 0, it returns False
+It's a concise way to say: "If the leaf node's value is 1, return True; otherwise (i.e., if it's 0), return False."
+'''
+
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
         if root.left is None and root.right is None:
