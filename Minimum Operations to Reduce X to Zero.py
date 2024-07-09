389d392bceef76d5b3ff0eff5f498da2ba0d8542
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-09 20:17:08 +0530

    Update Minimum Operations to Reduce X to Zero.py

--- Minimum Operations to Reduce X to Zero.py
+++ Minimum Operations to Reduce X to Zero.py
@@ -1,4 +1,23 @@
-
+# OPTIMAL SOLUTION -O(N)
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        target = sum(nums) - x
+        if target < 0:
+            return -1
+        if target == 0:
+            return len(nums)
+        
+        left = curr_sum = max_len = 0
+        
+        for right, num in enumerate(nums):
+            curr_sum += num
+            while curr_sum > target:
+                curr_sum -= nums[left]
+                left += 1
+            if curr_sum == target:
+                max_len = max(max_len, right - left + 1)
+        
+        return len(nums) - max_len if max_len > 0 else -1
 
 
 
