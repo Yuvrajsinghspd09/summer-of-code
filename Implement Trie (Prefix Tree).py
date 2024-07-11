51661b998f3f12f94ff8afd900e2219db4faa342
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-11 21:00:50 +0530

    Create Implement Trie (Prefix Tree).py

---
+++ Implement Trie (Prefix Tree).py
@@ -0,0 +1,51 @@
+# optimal approach
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+# brute force
+class Trie:
+    def __init__(self):
+        self.words = set()
+
+    def insert(self, word: str) -> None:
+        self.words.add(word)
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+
+    def startsWith(self, prefix: str) -> bool:
+        for word in self.words:
+            if word.startswith(prefix):
+                return True
+        return False
