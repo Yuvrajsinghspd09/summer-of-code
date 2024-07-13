4eff12b898065140a74021803baa220b9280073a
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-13 19:37:38 +0530

    Create Linked List Cycle.py

---
+++ Linked List Cycle.py
@@ -0,0 +1,24 @@
+# optimized
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow= head
+        fast =head
+        while fast and fast.next:
+            slow= slow.next
+            fast= fast.next.next
+            if slow==fast:
+                return True
+        return False
+
+
+# brute force
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        current = head
+        while current:
+            if current in visited:
+                return True
+            visited.add(current)
+            current= current.next
+        return False
