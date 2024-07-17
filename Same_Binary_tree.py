0992d55daddda09faa869ae2219bba1c34846498
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-17 19:11:32 +0530

    Update Same_Binary_tree.py

--- Same_Binary_tree.py
+++ Same_Binary_tree.py
@@ -21,3 +21,31 @@ class Solution:
         # Check the left and right subtrees recursively
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
+
+# conventional method 
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue = deque([(p, q)])
+        
+        while queue:
+            node1, node2 = queue.popleft()
+            
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+        
+        return True
