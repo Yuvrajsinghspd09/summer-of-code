07957b12a1ee97219d78eec2279b584505712170
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-19 14:05:15 +0530

    Update Next Greater Element.py

--- Next Greater Element.py
+++ Next Greater Element.py
@@ -16,3 +16,19 @@ def next_greater_element(arr):
 # Example usage
 arr = [4, 5, 2, 10, 8]
 print(next_greater_element(arr))  # Output: [5, 10, 10, -1, -1]
+
+
+#brute force
+def next_greater_element_brute(arr):
+    n = len(arr)
+    result = [-1] * n
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[j] > arr[i]:
+                result[i] = arr[j]
+                break
+    return result
+
+# Example usage
+arr = [4, 5, 2, 10, 8]
+print(next_greater_element_brute(arr))  # Output: [5, 10, 10, -1, -1]
