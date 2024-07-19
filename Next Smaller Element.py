ef955f03d75b1f064d15e9ff81b273d2bd74ed67
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-19 14:05:59 +0530

    Update Next Smaller Element.py

--- Next Smaller Element.py
+++ Next Smaller Element.py
@@ -13,3 +13,18 @@ def next_smaller_element(arr):
 # Example usage
 arr = [4, 5, 2, 10, 8]
 print(next_smaller_element(arr))  # Output: [2, 2, -1, 8, -1]
+
+#brute force
+def next_smaller_element_brute(arr):
+    n = len(arr)
+    result = [-1] * n
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[j] < arr[i]:
+                result[i] = arr[j]
+                break
+    return result
+
+# Example usage
+arr = [4, 5, 2, 10, 8]
+print(next_smaller_element_brute(arr))  # Output: [2, 2, -1, 8, -1]
