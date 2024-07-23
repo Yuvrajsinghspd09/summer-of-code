4a5abf9a4e4e4ba3610ca10cc5b7b9e498ac6602
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-23 11:25:42 +0530

    Create Asteroid Collision.py

---
+++ Asteroid Collision.py
@@ -0,0 +1,20 @@
+'''
+Explanation of the else Block:
+The else block is associated with the while loop, not the if statement. 
+In Python, a while loop can have an else clause which is executed only if the loop completes normally (i.e., it is not terminated by a break statement).
+'''
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+        return stack
