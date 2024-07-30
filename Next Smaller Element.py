f7850fecc988615339fb1a6ae0a8d88ba77537a8
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-07-30 21:59:38 +0530

    Update Next Smaller Element.py

--- Next Smaller Element.py
+++ Next Smaller Element.py
@@ -28,3 +28,17 @@ def next_smaller_element_brute(arr):
 # Example usage
 arr = [4, 5, 2, 10, 8]
 print(next_smaller_element_brute(arr))  # Output: [2, 2, -1, 8, -1]
+
+
+
+#practice attempt 1
+def nse(arr):
+    result = [-1]*len(arr)
+    stack=[]
+    for i in range(len(arr)):
+        while stack and arr[stack[-1]]>arr(i):
+            index = stack.pop
+            result[index]=arr(i)  
+
+        stack.append(i)
+    return result
