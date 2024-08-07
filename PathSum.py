31f16417c0c2280558cc0db59f7815106c2ebed0
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-08-07 21:56:04 +0530

    Update PathSum.py

--- PathSum.py
+++ PathSum.py
@@ -16,3 +16,22 @@ class Solution:
             return targetSum == 0
         
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+
+
+# practice attempt 1
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+      if not root:
+          return False
+     targetSum-=root.val
+
+    if not root.left and not root.right:
+        return targetSum==0
+
+     return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
