e22a2f950432b26eb5d42ab2a72c05901c1434b4
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-10-29 21:13:17 +0530

    Update Valid Palindrome.py

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -9,3 +9,39 @@ class Solution:
             cleaned+=char.lower()
         
        return cleaned ==cleaned[::-1]
+
+
+#practice attempt 1 with 2 pointer approach
+class Solution:
+    def isPalindrome(self,s):
+        left=0
+        right=len(s)-1
+        while left <right :
+            if s[left] != s[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+
+#practice attempt 2 with 2 pointer approach
+class Solution:
+    def is_palindrome(self,s):
+        if len(s)==0 or len(s)==1:
+            return True
+        if len(s)>1:
+            left=0
+            right=len(s)-1
+            while left<right:
+                if s[left]==s[right]:
+                    left+=1
+                    right-=1
+                else:
+                    return False
+            return True
+        return False
+            
+    return False
+
+abcba
+        
+            
