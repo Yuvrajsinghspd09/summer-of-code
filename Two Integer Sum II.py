daaa0881191498558605e47640e96cf9b331d4dd
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-10-31 01:31:03 +0530

    Update Two Integer Sum II.py

--- Two Integer Sum II.py
+++ Two Integer Sum II.py
@@ -9,3 +9,18 @@ class Solution:
                 left+=1
             else:
                 right-=1
+
+#practice attempt 1
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left=0
+        right=len(numbers)-1
+        while left<right:
+            total = numbers[left]+numbers[right]
+            if total==target:
+                return left+1,right+1
+            elif total>target:
+                right-=1
+            else:
+                left+=1
+        return False
