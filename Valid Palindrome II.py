317e6d9e2c77ebd3d54ebb2b6112e82ab7eb7f58
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-11-07 12:24:09 +0530

    Update Valid Palindrome II.py

--- Valid Palindrome II.py
+++ Valid Palindrome II.py
@@ -1,11 +1,24 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-            left=0
-            right = len(s)-1
-            while left<right:
-                if s[left]==s[right]:
-                    left+=1
-                    right-=1
-                else:
-                    return s[left:right]==s[left:right][::-1] or s[left:right+1]==s[left:right+1][::-1] 
-            return True
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                # Check by skipping either the left or the right character
+                return s[left+1:right+1] == s[left+1:right+1][::-1] or s[left:right] == s[left:right][::-1]
+        return True
+
+
+#practice attempt 1
+ l=0
+ r=len(s)-1
+while l<r:
+    if s[l]==s[r]:
+        l+=1
+        r-=1
+    else:
+        return s[l+1:r+1]==s[l+1:r+1] or s[l:r]==s[l:r]
+return True
