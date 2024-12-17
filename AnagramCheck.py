1333f224fa3335a5ca7461f9d196f31e2d2fa0b9
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-12-17 18:47:35 +0530

    Update AnagramCheck.py

--- AnagramCheck.py
+++ AnagramCheck.py
@@ -5,6 +5,13 @@ def isAnagram(s, t):
 An anagram is formed by rearranging the letters of a word or phrase.
  To check if two strings are anagrams, we can compare their sorted versions
 '''
-# practice attempt 1
-def anagram_check(word1,word2):
-    return sorted(word1)==sorted(word2)
+# optimal approach
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        count= [0]*26
+        for i in range(len(s)): # or for i in range(len(t))
+            count[ord(s[i])-ord('a')]+=1
+            count[ord(t[i])-ord('a')]-=1
+        return all( x==0 for x in count)
